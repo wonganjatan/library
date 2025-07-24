25265042bd84731150b6436c5150ed6f18ded885
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2025-07-25 09:10:17 +1000

    Added constructor

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const myLibrary = []
 
-function Book() {
-
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
 }
 
 function addBookToLibrary() {
-    
+
 }
