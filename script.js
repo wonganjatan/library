083c4bdb7fa66e4d0c269b98056ebdd33a6c6e03
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2025-07-25 09:28:20 +1000

    Added submit button

--- script.js
+++ script.js
@@ -7,18 +7,18 @@ function Book(title, author, pages, read) {
     this.read = read
 }
 
-function addBookToLibrary() {
-    const title = prompt("Title?")
-    const author = prompt("Author?")
-    const pages = prompt("Number of Page?")
-    const read = confirm("Have you read it?")
+// function addBookToLibrary() {
+//     const title = prompt("Title?")
+//     const author = prompt("Author?")
+//     const pages = prompt("Number of Page?")
+//     const read = confirm("Have you read it?")
 
-    const newBook = new Book(title, author, pages, read)
-    myLibrary.push(newBook)
-}
+//     const newBook = new Book(title, author, pages, read)
+//     myLibrary.push(newBook)
+// }
 
-addBookToLibrary()
+// addBookToLibrary()
 
-myLibrary.forEach(book => {
-    console.log(book)
-});
+// myLibrary.forEach(book => {
+//     console.log(book)
+// });
